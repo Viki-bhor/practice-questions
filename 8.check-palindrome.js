240e6758793a8b183e7607f89bdaccd4f282cb04
viki <vikrantbhor33@gmail.com>
2025-08-01 12:18:19 +0530

    another method to check palindrome added

--- 8.check-palindrome.js
+++ 8.check-palindrome.js
@@ -19,5 +19,17 @@ function isPalindrome2(inputString) {
    return true;
 }
 
+function isPalindrome3(inputString) {
+   let start = 0;
+   let end = inputString.length - 1;
+   for(start, end; start < end; start++, end--) {
+      if(inputString[start] !== inputString[end]) {
+         return false;
+      }
+      return true;
+   }
+}
+
 console.log(isPalindrome('madam'));
 console.log(isPalindrome2('racecar'));
+console.log(isPalindrome3('nice'));
